c56191e5a65d6c1380dbdea41868c080c29c6d9b
The Awesome <mohsindude5@gmail.com>
2023-10-09 00:48:12 +0600

    added profile

--- src/pages/UserPages/Profile.jsx
+++ src/pages/UserPages/Profile.jsx
@@ -16,13 +16,14 @@ const Profile = () => {
             <div className="hero">
                 <div className="hero-content flex-col lg:flex-row">
                     {
-                        user.photoURL !== null ? <img src={user.photoURL} /> : <img className="rounded-full" src="/icons/user.png" />
+                        user.photoURL !== null ? <img className="rounded-full h-48" src={user.photoURL} /> : <img className="rounded-full" src="/icons/user.png" />
                     }
-                    <img src={user.photoURL} className="max-w-sm rounded-lg shadow-2xl" />
-                    <div>
+                    
+                    <div className="text-center lg:text-left pt-10 lg:pt-0 lg:pl-10">
                         <h1 className="text-5xl font-bold">{user.displayName}</h1>
-                        <p className="py-6">Email: {user.email}</p>
+                        <p className="py-6 font-semibold text-2xl">Email: {user.email}</p>
                         <p className="">Unique ID: {user.uid}</p>
+                        <p className="pt-2">Last Login: {user.metadata.lastSignInTime}</p>
 
                     </div>
                 </div>
